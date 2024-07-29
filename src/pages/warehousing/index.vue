dddef03c488060837f3c812720bb3f39a39eb503
molianglcl <molianglcl@gmail.com>
2024-07-29 17:31:23 +0800

    feat：添加黑白背景

--- src/pages/warehousing/index.vue
+++ src/pages/warehousing/index.vue
@@ -12,7 +12,7 @@ definePage({
 </script>
 
 <template>
-  <div class="bg wh-full flex-col">
+  <div class="bw-bg wh-full flex-col">
     <Header title="重要实物出库" user-name="龙傲天" user-id="12315556456" />
     WarehousingPage111
   </div>
